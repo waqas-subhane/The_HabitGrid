7dc0c3773eea97b05e695a5753cb949bc6200a74
waqas-subhane <waqassubhani99@gmail.com>
2026-08-29 11:51:43 +0530

    jahvef

--- backend/server.ts
+++ backend/server.ts
@@ -518,9 +518,9 @@ async function startServer() {
   } else {
     const distPath = path.join(process.cwd(), 'dist');
     app.use(express.static(distPath));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(distPath, 'index.html'));
-    });
+    app.use((req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
   }
 
   app.listen(PORT, '0.0.0.0', () => {
